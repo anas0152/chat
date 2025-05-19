da44b23657468be65bb801eee9972b134e4b2f6c
Anas Hantouti <pizzaroyal28000@gmail.com>
2025-05-19 16:15:58 +0200

    frank le neuil

---
+++ resources/views/cgu.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Conditions Générales d’Utilisation</title>
+</head>
+<body>
+    <h1>Conditions Générales d’Utilisation</h1>
+    <p>Bienvenue sur notre application. Voici les conditions générales d’utilisation (CGU)...</p>
+    <!-- Tu peux coller ici le texte complet de tes CGU -->
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,10 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/cgu', function () {
+    return view('cgu');
+});
+
+Route::get('/cgu', [App\Http\Controllers\PageController::class, 'cgu']);
+
 require __DIR__.'/auth.php';
